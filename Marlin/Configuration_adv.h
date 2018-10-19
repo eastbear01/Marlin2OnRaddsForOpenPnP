85229a149c550b3086b40f9b3d62dffa5178d47a
michael <michael@groene.de>
2018-10-19 21:14:28 +0200

    fixed MIN_STEPS_PER_SEGMENT

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -901,7 +901,7 @@
 #endif
 
 // Moves (or segments) with fewer steps than this will be joined with the next move
-#define MIN_STEPS_PER_SEGMENT 0
+#define MIN_STEPS_PER_SEGMENT 1
 
 /**
  * Minimum delay after setting the stepper DIR (in ns)
